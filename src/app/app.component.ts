fa2628f01685779d341b1e39cadefa0e06b71e7c
Pranav ANEJA <PAneja@mycwt.com>
2020-03-11 21:13:36 +0530

    Main code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,99 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'health-care-system';
+  disableLoginButtons = false;
+  showImage = true;
+  loginType = '';
+  dataType = '';
+  treatments = [
+    {
+      name : 'Blood Pressure',
+      amount: '300',
+      currency: 'CAD'
+    },
+    {
+      name : 'Haemoglobin',
+      amount: '400',
+      currency: 'CAD'
+    },
+    {
+      name : 'Diabetes',
+      amount: '1000',
+      currency: 'CAD'
+    },
+    {
+      name : 'ECG',
+      amount: '1300',
+      currency: 'CAD'
+    }
+  ];
+  patientsList = [
+    {
+      firstName: 'Pranav',
+      lastName: 'Aneja',
+      age: '24',
+      sex: 'Male',
+      id: 'D1234'
+    },
+    {
+      firstName: 'Puneet',
+      lastName: 'Aggarwal',
+      age: '24',
+      sex: 'Male',
+      id: 'F1235'
+    },
+    {
+      firstName: 'Leslie',
+      lastName: 'Williams',
+      age: '24',
+      sex: 'Female',
+      id: 'E1236'
+    },
+    {
+      firstName: 'Aditi',
+      lastName: 'Sharma',
+      age: '23',
+      sex: 'Female',
+      id: 'D1237'
+    },
+    {
+      firstName: 'Shreya',
+      lastName: 'Sood',
+      age: '22',
+      sex: 'Female',
+      id: 'M1238'
+    },
+    {
+      firstName: 'Mishika',
+      lastName: 'Arora',
+      age: '25',
+      sex: 'Female',
+      id: 'S1231'
+    },
+    {
+      firstName: 'Rishi',
+      lastName: 'Trehan',
+      age: '24',
+      sex: 'Male',
+      id: 'S1335'
+    }
+  ]
+  openLoginPhysician() {
+    this.loginType = 'physician';
+    this.showImage = false;
+  }
+  openLoginPatient() {
+    this.loginType = 'patient';
+    this.showImage = false;
+  }
+  showPhysiciansDetails() {
+    this.loginType = '';
+    this.disableLoginButtons = true;
+    this.dataType = 'physician';
+  }
+  showPatientsDetails() {
+    this.loginType = '';
+    this.disableLoginButtons = true;
+    this.dataType = 'patient';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhysicianComponent } from './components/physician/physician.component';
+import { PatientComponent } from './components/patient/patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhysicianComponent,
+    PatientComponent
   ],
   imports: [
     BrowserModule
